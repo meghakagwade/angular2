9e478e2fd14419c7a116ec24c987d5235282dea1
Megha Wadkar <wadkarmegha97@gmail.com>
2020-12-02 11:43:28 -0500

    first commit

---
+++ APM-Start/src/app/products/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { catchError,tap }  from 'rxjs/Operators';
+
+import{ IProduct }  from './Product';
+
+@Injectable({
+    providedIn:'root'
+})
+export class ProductService {
+    private productUrl='api/products/products.json';
+    constructor(private http:HttpClient){}
+     
+   getProducts(): Observable<IProduct[]>{
+       return  this.http.get<IProduct[]>(this.productUrl).pipe(
+           tap(data=> console.log('All: '+ JSON.stringify(data))),catchError(this.handleError)
+       );
+   }
+    handleError(handleError: any): import("rxjs").OperatorFunction<IProduct[], any> {
+        throw new Error('Method not implemented.');
+    }
+}
---
+++ APM-Start/src/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct{
+    productId:number;
+    productName:String;
+    productCode:String;
+    releaseDate:String;
+    price:number;
+    description:string;
+    starRating:number;
+    imageUrl:String
+}
